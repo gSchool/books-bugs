0011fc2cc0e143852220f4adab0ac27088275ec1
marthaberner <marthaberner.mb@gmail.com>
2016-01-17 15:05:28 -0700

    add bugs

--- routes/books.js
+++ routes/books.js
@@ -1,27 +1,48 @@
 var express = require('express');
 var router = express.Router();
-var knex = require('../db/knex');
 
-function Books() {
+function books() {
   return knex('books');
 }
 
-router.get('/books', function(req, res, next) {
+router.get('/', function(req, res, next) {
   Books().select().then(function (books) {
     res.render('books/index', {books: books});
   })
 });
 
-router.get('/books/new', function(req, res, next) {
+router.post('/', function (req, res, next) {
+  Books().insert(req.body).then(function (results) {
+    res.redirect('/books');
+  })
+})
+
+router.get('/new', function(req, res, next) {
   res.render('books/new');
 });
 
-router.get('/books/show', function(req, res, next) {
-  res.render('books/show', {book: book});
+router.get('/:id', function(req, res, next) {
+  Books().where('id', req.params.id).first().then(function (book) {
+    res.render('books/show', {book: book});
+  });
 });
 
-router.get('/books/edit', function(req, res, next) {
-  res.render('books/edit', {book: book});
+router.get('/:id/edit', function(req, res, next) {
+  Books().where('id', req.params.id).first().then(function (book) {
+    res.render('books/edit', {book: book});
+  });
 });
+
+router.post('/:id', function (req, res, next) {
+  Books().where('id', req.params.id).update(req.body).then(function (results) {
+    res.redirect('/books');
+  })
+});
+
+router.post('books/:id/delete', function (req, res, next) {
+  Books().where('id', req.params.id).del().then(function (results) {
+    res.redirect('/books');
+  })
+})
 
 module.exports = router;

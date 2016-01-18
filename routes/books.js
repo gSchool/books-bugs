1c4cd955e6a9f2903f3a45b9f64c87dce3cc9851
Teisia Park <teisia@gmail.com>
2016-01-18 15:42:00 -0700

    delete works

--- routes/books.js
+++ routes/books.js
@@ -40,9 +40,9 @@ router.post('/:id', function (req, res, next) {
   })
 });
 
-router.post('books/:id/delete', function (req, res, next) {
+router.post('/:id/delete', function (req, res, next) {
   Books().where('id', req.params.id).del().then(function (results) {
-    res.redirect('/books');
+    res.redirect('/');
   })
 })
 

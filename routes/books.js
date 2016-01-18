1f9142782d59d96c97809136945b049be8240025
Ben Ebel <bge1234@gmail.com>
2016-01-18 13:52:54 -0700

    finished bug-2

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var knex = require('../db/knexfile');
+var knex = require('../db/knex');
 
 function Books() {
   return knex('books');
@@ -30,19 +30,20 @@ router.get('/books/:id', function(req, res, next) {
 
 router.get('/books/:id/edit', function(req, res, next) {
   Books().where('id', req.params.id).then(function (book) {
-    res.render('books/edit', {book: book});
+    console.log(book);
+    res.render('books/edit', {book: book[0]});
   });
 });
 
 router.post('/books/:id', function (req, res, next) {
   Books().where('id', req.params.id).update(req.body).then(function (results) {
     res.redirect('/books');
-  })
+  });
 });
 
 router.post('/books/:id/delete', function (req, res, next) {
   Books().where('id', req.params.id).del().then(function (results) {
-    res.redirect('books', {books: results});
+    res.redirect('/books');
   })
 })
 

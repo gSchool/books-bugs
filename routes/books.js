4c6e7d700dccce2ce49ea4450b2d2675927afcb9
Teisia Park <teisia@gmail.com>
2016-01-18 15:31:48 -0700

    can delete books holla

--- routes/books.js
+++ routes/books.js
@@ -42,7 +42,7 @@ router.post('/books/:id', function (req, res, next) {
 
 router.post('/books/:id/delete', function (req, res, next) {
   Books().where('id', req.params.id).del().then(function (results) {
-    res.redirect('books', {books: results});
+    res.redirect('/books');
   })
 })
 

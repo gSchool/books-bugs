8c6e5c5b3181c737c2c23352b8a2a5f553ea1a46
Stuart Urback <stuart@urback.net>
2016-01-18 18:33:04 -0700

    Rename knex requirement in book.js route

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var knex = require('../db/knexfile');
+var knex = require('../db/knex');
 
 function Books() {
   return knex('books');

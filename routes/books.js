e7c5aeb144b6cf613ecd1a1569b240b62998986c
Stuart Urback <stuart@urback.net>
2016-01-18 18:41:56 -0700

    Update edit single knex call to look for first row with id so that knex returns an object instead of an array

--- routes/books.js
+++ routes/books.js
@@ -29,7 +29,8 @@ router.get('/books/:id', function(req, res, next) {
 });
 
 router.get('/books/:id/edit', function(req, res, next) {
-  Books().where('id', req.params.id).then(function (book) {
+  Books().where('id', req.params.id).first().then(function(book) {
+    console.log(book);
     res.render('books/edit', {book: book});
   });
 });
